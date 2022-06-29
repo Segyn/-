2ccc8d54f39e7f537cad4d5cf631b27f94499f7e
ak10s <ak10s@mail.ru>
2022-06-29 22:17:46 +0500

    добавлен счетчик

--- Game/__init__.py
+++ Game/__init__.py
@@ -5,13 +5,19 @@ import random
 def game100() -> str:
     random_num = random.randint(0, 100)
     print(random_num)
-    user_num = int(input('Введите число '))
-    if random_num > user_num:
-        print('Ваше число больше')
-    elif random_num < user_num:
-        print('Ваше число меньше')
-    else:
-        return print('Вы угадали')
+    
+    i = 0
+    while i < 10:
+        user_num = int(input('Введите число '))
+        if random_num < user_num:
+            print('Ваше число больше')
+            i += 1
+        elif random_num > user_num:
+            print('Ваше число меньше')
+            i += 1
+        else:
+            print('Вы угадали')
+            break
 
 
 if __name__ == '__main__':
