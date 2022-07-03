5024d455507cfccb78cdfd93f02f122ea6ca300e
ak10s <ak10s@mail.ru>
2022-07-03 13:12:18 +0500

    добавлен текстовый вывод счетчика

--- Game/__init__.py
+++ Game/__init__.py
@@ -5,21 +5,22 @@ import random
 def game100() -> str:
     random_num = random.randint(0, 100)
     print(random_num)
-    
+    print('Угадайте число использовав десять попыток\n')
     i = 0
     while i < 10:
         i+=1
         user_num = int(input('Введите число '))
         if random_num < user_num:
-            print('Ваше число больше')
+            print('Ваше число больше загаданного')
+
             
         elif random_num > user_num:
-            print('Ваше число меньше')
+            print('Ваше число меньше загаданного')
             
         else:
             print('Вы угадали')
             break
-
+        print(f'Вы использовали {i} попыток из десяти\n')
 
 if __name__ == '__main__':
     game100()
