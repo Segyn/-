5f86d6aac9e1a6876acaf8ecb037d0ca14d49213
ak10s <ak10s@mail.ru>
2022-07-06 22:35:07 +0500

    вынесен счетчик в глобальную переменную

--- Game/function.py
+++ Game/function.py
@@ -1,13 +1,19 @@
 import random
 import text
 
+# Счетчик попыток
+counter_try = 0
 
+# Ввод числа пользователем и проверка ввода
 def inner_user_num():
     user_num = input('Введите целое положительное число: -> ')
     if user_num.isdigit():
         return int(user_num)
     else:
         print('\nВы ввели не целое положительное число\n')
+        global counter_try
+        counter_try+=1
+        print(f'Вы использовали {text.text_num_try[counter_try-1]} попытку из десяти')
         return inner_user_num()
 
 
@@ -15,10 +21,10 @@ def game100():
     random_num = random.randint(0, 100)
     print(random_num)
     print('\nУгадайте целое положительное число от нуля до ста, использовав десять попыток.\n')
+    global counter_try
 
-    i = 0
-    while i < 10:
-        i += 1
+    while counter_try < 10:
+        counter_try += 1
         user_num = inner_user_num()
 
         if random_num < user_num:
@@ -29,9 +35,9 @@ def game100():
 
         else:
             return print(f'\nВы угадали загаданное число {random_num}, '
-                         f'использовав {text.text_num_vin[i]} {text.text_num_end[i]}')
+                         f'использовав {text.text_num_vin[counter_try]} {text.text_num_end[counter_try]}')
 
-        print(f'Вы использовали {text.text_num_try[i]} попытку из десяти\n')
+        print(f'Вы использовали {text.text_num_try[counter_try]} попытку из десяти\n')
     return print(f'Вы использовали все попытки, загаданное число было: {random_num}')
 
 
