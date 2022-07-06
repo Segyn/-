9060e28eebee958cda426ee3d15055c028f51514
ak10s <ak10s@mail.ru>
2022-07-06 22:48:43 +0500

    перенос названия игры комментарии

--- Game/function.py
+++ Game/function.py
@@ -1,8 +1,11 @@
 import random
 import text
 
+# Название игры
+print('\nУгадайте целое положительное число от нуля до ста, использовав десять попыток.\n')
 # Счетчик попыток
 counter_try = 0
+
 
 # Ввод числа пользователем и проверка ввода
 def inner_user_num():
@@ -10,17 +13,17 @@ def inner_user_num():
     if user_num.isdigit():
         return int(user_num)
     else:
-        print('\nВы ввели не целое положительное число\n')
+        print('\nВы ввели не целое положительное число!')
         global counter_try
-        counter_try+=1
-        print(f'Вы использовали {text.text_num_try[counter_try-1]} попытку из десяти')
+        counter_try += 1
+        print(f'Вы использовали {text.text_num_try[counter_try - 1]} попытку из десяти\n')
         return inner_user_num()
 
 
+# Основная функция игры
 def game100():
     random_num = random.randint(0, 100)
     print(random_num)
-    print('\nУгадайте целое положительное число от нуля до ста, использовав десять попыток.\n')
     global counter_try
 
     while counter_try < 10:
@@ -28,10 +31,10 @@ def game100():
         user_num = inner_user_num()
 
         if random_num < user_num:
-            print('Ваше число больше загаданного')
+            print('\nВаше число больше загаданного')
 
         elif random_num > user_num:
-            print('Ваше число меньше загаданного')
+            print('\nВаше число меньше загаданного')
 
         else:
             return print(f'\nВы угадали загаданное число {random_num}, '
